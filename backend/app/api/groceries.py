64d1fd3764ae0d5fd98b6f82e6f629c4aebfd95c
Jason Force <mojason-81@users.noreply.github.com>
2018-12-07 23:02:41 -0600

    Successfully add grocery and update table in UI (#2)
    - Adding "add grocery" functionality
      - Fix getStores callback
      - Plop in table, plop in data for groceries
      - Don't divide grocery price in dict
      - Add spoofed groceries POST
      - TODO:
        - Add form to UI for grocery data

--- backend/app/api/groceries.py
+++ backend/app/api/groceries.py
@@ -22,3 +22,24 @@ def get_groceries(id):
 def get_grocery(user_id, id):
     # FIXME: only return grocery if it belongs to user.
     return jsonify(Grocery.query.get_or_404(id).to_dict())
+
+# May have to POST to /groceries and include the user id as form data
+@bp.route('/users/<int:id>/groceries', methods=['POST'])
+@token_auth.login_required
+def create_grocery(id):
+    data = request.get_json() or {}
+    # FIXME: it's failing.  find out why
+    if 'name' not in data:
+        return bad_request('must include name field.')
+    grocery = Grocery()
+    grocery.from_dict(data)
+    g.current_user.add_grocery(grocery)
+    response = jsonify(grocery.to_dict())
+    response.status_code = 201
+    # TODO: Will grocery here get an id after commit() or do
+    # I need to look it up again?
+    response.headers['Location'] = url_for('api.get_grocery',
+                                           user_id=g.current_user.id,
+                                           id=grocery.id)
+    return response
+
--- frontend/src/components/Groceries.vue
+++ frontend/src/components/Groceries.vue
@@ -5,8 +5,50 @@
         <div class="card">
           <div class="card-content">
             <p class="title"></p>
+            <p class="subtitle">Add a button to "Add Grocery"</p>
             <!-- TODO: Display pretty data -->
             <p>{{ msg }}</p>
+            <!--
+            <hr>
+            <p>{{ groceryData }}</p>
+            <hr>
+            <p>{{ storeData }}</p>
+            <hr>
+            -->
+            <hr>
+            <table id='grocery-table' class='table is-striped is-hoverable'>
+              <th class='th'>
+                Name
+              </th>
+              <th class='th'>
+                Price
+              </th>
+              <th class='th'>
+                Count
+              </th class='th'>
+              <tr v-for="item in groceryData.items" class="tr">
+                <td class="td">
+                  {{ item.name }}
+                </td>
+                <td class="td">
+                  ${{ item.price / 100 }}
+                </td>
+                <td class="td">
+                  {{ item.count }}
+                </td>
+              </tr>
+            </table>
+            <hr>
+            <div :class="display">
+              TODO: Grocery form goes here<br>
+              TODO: Add multiple groceries at once.
+              <button v-on:click="addGrocery()">
+                Faux submit button
+              </button>
+            </div>
+            <button v-on:click="exposeGroceryForm()">
+              Add Grocery
+            </button>
           </div>
         </div>
       </div>
@@ -19,12 +61,20 @@ export default {
   name: 'Home',
   data() {
     return {
-      msg: 'Home screen for a user will be and entry point to the app doing stuff and such.'
+      msg: 'Home screen for a user will be an entry point to the app doing stuff and such I think maybe.',
+      groceryData: '',
+      storeData: '',
+      display: 'is-hidden',
+      grocery_name: 'rice',
+      grocery_price: '1.49',
+      grocery_count: '1',
+      store_id: '1'
     }
   },
+
   methods: {
     getGroceries() {
-      console.log(sessionStorage.blah);
+      // maybe post to something like /users/:id/:store_id/groceries
       this.$http({
         method: 'get',
         url: `/users/${localStorage.current_user_id}/groceries`,
@@ -32,10 +82,66 @@ export default {
           'Authorization': 'Bearer ' + localStorage.token
         }
       })
-        .then((response) => { this.msg = response.data })
-        .catch( error => { console.log(error) })
+        .then((response) => { this.setGroceryData(response) })
+        .then(() => { this.getStores() })
+        .catch((e) => this.flashSomeKindOfError(e))
+    },
+
+    addGrocery() {
+      this.$http({
+        method: 'post',
+        url: `/users/${localStorage.current_user_id}/groceries`,
+        headers: {
+          'Authorization': 'Bearer ' + localStorage.token
+        },
+        data: {
+          name: this.grocery_name,
+          price: this.grocery_price,
+          count: this.grocery_count,
+          store_id: this.store_id
+        }
+      })
+        .then((res) => this.addToGroceryData(res))
+        .catch((e) => this.flashSomeKindOfError(e))
+    },
+
+    // The data here should probably be computed properties.
+    // Hopefully it will refresh on page refresh
+    getStores() {
+      var uid = localStorage.current_user_id;
+      var tkn = localStorage.token;
+      this.$http({
+        method: 'get',
+        url: `/users/${localStorage.current_user_id}/stores`,
+        headers: {
+          'Authorization': 'Bearer ' + localStorage.token
+        }
+      })
+        .then((response) => this.setStoreData(response))
+        .catch((e) => this.flashSomeKindOfError(e))
+    },
+
+    setStoreData(res) {
+      this.storeData = res.data
+    },
+
+    setGroceryData(res) {
+      this.groceryData = res.data
+    },
+
+    addToGroceryData(res) {
+      this.groceryData.items.push(res.data)
+    },
+
+    flashSomeKindOfError(e) {
+      alert("Something failed:\n" + e + `\n${localStorage.token}`)
+    },
+
+    exposeGroceryForm() {
+      this.display = ''
     }
   },
+
   created() {
     sessionStorage.blah = "Consider using session storage for token and user_id.\nThat might be deleted if you close your browser though.  Maybe useful for a remember_me feature."
     this.getGroceries()
